9ea78355218755c2080ccf862ec7dd9a6ec0fb89
igor <ceigor94@gmail.com>
2019-07-20 08:39:01 +0200

    Active record tests

--- src/test/java/com/iprogrammerr/smart/query/meta/ActiveRecordTest.java
+++ src/test/java/com/iprogrammerr/smart/query/meta/ActiveRecordTest.java
@@ -11,13 +11,13 @@ import org.junit.Test;
 
 public class ActiveRecordTest {
 
-    private Database database;
     private QueryFactory factory;
 
     @Before
     public void setup() throws Exception {
         Configuration configuration = Configuration.fromCmd();
-        database = new Database(configuration.jdbcUrl, configuration.databaseUser, configuration.databasePassword);
+        Database database = new Database(configuration.jdbcUrl, configuration.databaseUser,
+            configuration.databasePassword);
         factory = new SmartQueryFactory(database::connection, false);
         database.setup();
     }
@@ -35,9 +35,47 @@ public class ActiveRecordTest {
 
         int id = (int) ar.insertReturningId();
 
-        Author a = ar.fetch();
+        MatcherAssert.assertThat(ar.fetch(), Matchers.equalTo(new Author(id, name, surname, alias, alive)));
+    }
 
-        MatcherAssert.assertThat(a, Matchers.equalTo(new Author(id, name, surname, alias, alive)));
+
+    @Test
+    public void updates() {
+        String name = "Stanisław";
+        String surname = "Lem";
+        String alias = "Futurysta";
+        int alive = 0;
+
+        AuthorRecord ar = new AuthorRecord(factory)
+            .setName(name).setSurname(surname)
+            .setAlias(alias).setAlive(alive);
+
+        int id = (int) ar.insertReturningId();
+        String newName = "Stasiek";
+        int newAlive = 1;
+        ar.setName(newName);
+        ar.setAlive(newAlive);
+
+        ar.update();
+
+        MatcherAssert.assertThat(ar.fetch(), Matchers.equalTo(new Author(id, newName, surname, alias, newAlive)));
+    }
+
+    @Test
+    public void deletes() {
+        AuthorRecord ar = new AuthorRecord(factory)
+            .setName("a").setSurname("b")
+            .setAlias("c").setAlive(1);
+
+        long id = ar.insertReturningId();
+        ar.delete();
+
+        boolean noRecords = factory.newQuery()
+            .sql("SELECT id FROM author WHERE id = ?")
+            .set(id)
+            .fetch(r -> !r.next());
+
+        MatcherAssert.assertThat(noRecords, Matchers.equalTo(true));
     }
 
 }
